9aa5566198b7d4e0dec10def38b9bf6b09ee516b
Finn <71390226+FinnPaes@users.noreply.github.com>
2024-03-03 16:04:26 +0100

    Add search functionality

--- app/Livewire/App/Websites/WebsiteList.php
+++ app/Livewire/App/Websites/WebsiteList.php
@@ -3,13 +3,18 @@
 namespace App\Livewire\App\Websites;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\View\View;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class WebsiteList extends Component
 {
     use WithPagination;
+
+    #[Url]
+    public string $search = '';
 
     public function render(): View
     {
@@ -20,6 +25,7 @@ class WebsiteList extends Component
         $websites = $user->activeTeam
             ->websites()
             ->with('monitorEntriesLatest')
+            ->when(! blank($this->search), fn (Builder $query): Builder => $query->where('domain', 'like', "%$this->search%")->orWhere('name', 'like', "%$this->search%"))
             ->paginate(4);
 
         return view('livewire.app.websites.website-list', [
--- resources/views/livewire/app/websites/website-list.blade.php
+++ resources/views/livewire/app/websites/website-list.blade.php
@@ -1,8 +1,19 @@
 <div class="w-full p-5 space-y-8" wire:poll.10s>
+    <div class="flex items-center justify-between">
+        <div></div>
+        <div>
+            <x-form.input :placeholder="__('Search')" wire:model.live="search" />
+        </div>
+    </div>
+
     @forelse ($websites as $website)
         <x-website-row :website="$website" />
     @empty
-        <x-no-records icon="heroicon-o-plus" :text="__('Add a new website')" wire:click="$parent.$toggle('showWebsiteCreation')" />
+        @if (blank($search))
+            <x-no-records icon="heroicon-o-plus" :text="__('Add a new website')" wire:click="$parent.$toggle('showWebsiteCreation')" />
+        @else
+            <x-no-records icon="heroicon-o-plus" :text="__('Website \':search\' not found, do you want to create it?', ['search' => $search])" wire:click="$parent.$toggle('showWebsiteCreation')" />
+        @endif
     @endforelse
 
     {{ $websites->links() }}
